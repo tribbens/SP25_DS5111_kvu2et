6cb3c67998f65b5004f3f8f7929bd61bef9b5dfb
tribbens <tribbens@live.com>
2025-02-07 23:52:13 +0000

    create init file

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt update # To bring VM snapshot up to date with package versions
+sudo apt install make -y # so we can use makefiles
+sudo apt install python3.12-venv -y # so we can create python virtual environments
+sudo apt install tree # a usefull tool for listing files in tree form
