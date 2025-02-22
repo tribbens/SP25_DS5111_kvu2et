57993c171d982097190dcd43424e6f4d73a6d6a2
tribbens <tribbens@live.com>
2025-02-22 01:38:04 +0000

    revisions for incorrect inputs

--- bin/normalize_csv.py
+++ bin/normalize_csv.py
@@ -5,9 +5,8 @@ print(f'variables passed: {sys.argv}')
 raw_csv_file = sys.argv[1].strip()
 
 def normalize_csv(raw_csv_path):
-    raw_df = pd.read_csv(raw_csv_path)
-    
     if 'ygainers' in raw_csv_path:
+        raw_df = pd.read_csv(raw_csv_path)
         print('ygainers loaded')
         norm_df = raw_df[['Symbol', 'Price', 'Change', 'Change %']]
         norm_df.columns = ['symbol', 'price', 'price_change', 'price_percent_change']
@@ -16,6 +15,7 @@ def normalize_csv(raw_csv_path):
         return norm_df.to_csv('ygainers_norm.csv', index=False)
     
     elif 'wjsgainers' in raw_csv_path:
+        raw_df = pd.read_csv(raw_csv_path)
         print('wjsgainers loaded')
         norm_df = raw_df[['Unnamed: 0', 'Last', 'Chg', '% Chg']]
         norm_df.columns = ['symbol', 'price', 'price_change', 'price_percent_change']
