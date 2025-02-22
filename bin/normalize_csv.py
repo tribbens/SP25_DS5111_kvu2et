36d8f74a378581d9abc83065da20cf211da42d21
tribbens <tribbens@live.com>
2025-02-22 07:49:01 +0000

    revisions from pylint results

--- bin/normalize_csv.py
+++ bin/normalize_csv.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import sys
+import pandas as pd
 assert len(sys.argv)==2, "too many inputs, expects 2"
 print(f'variables passed: {sys.argv}')
 raw_csv_file = sys.argv[1].strip()
@@ -10,10 +10,10 @@ def normalize_csv(raw_csv_path):
         print('ygainers loaded')
         norm_df = raw_df[['Symbol', 'Price', 'Change', 'Change %']]
         norm_df.columns = ['symbol', 'price', 'price_change', 'price_percent_change']
-        
+ 
         assert type(norm_df) == pd.DataFrame
         return norm_df.to_csv('ygainers_norm.csv', index=False)
-    
+
     elif 'wsjgainers' in raw_csv_path:
         raw_df = pd.read_csv(raw_csv_path)
         print('wsjgainers loaded')
@@ -21,10 +21,10 @@ def normalize_csv(raw_csv_path):
         norm_df.columns = ['symbol', 'price', 'price_change', 'price_percent_change']
         # extract only the symbol
         norm_df['symbol'] = norm_df['symbol'].str.extract(r'\((.*?)\)')
-        
+
         assert type(norm_df) == pd.DataFrame
         return norm_df.to_csv('wsjgainers_norm.csv', index=False)
-    
+
     else:
         print('Please enter a supported file')
 
