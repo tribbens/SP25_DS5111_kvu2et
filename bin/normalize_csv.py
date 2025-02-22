385e399ed7e4402e77b8114f0b51ca8fc4337df7
tribbens <tribbens@live.com>
2025-02-22 07:31:48 +0000

    revising naming to be wsjgainers instead of wjsgainers

--- bin/normalize_csv.py
+++ bin/normalize_csv.py
@@ -14,16 +14,16 @@ def normalize_csv(raw_csv_path):
         assert type(norm_df) == pd.DataFrame
         return norm_df.to_csv('ygainers_norm.csv', index=False)
     
-    elif 'wjsgainers' in raw_csv_path:
+    elif 'wsjgainers' in raw_csv_path:
         raw_df = pd.read_csv(raw_csv_path)
-        print('wjsgainers loaded')
+        print('wsjgainers loaded')
         norm_df = raw_df[['Unnamed: 0', 'Last', 'Chg', '% Chg']]
         norm_df.columns = ['symbol', 'price', 'price_change', 'price_percent_change']
         # extract only the symbol
         norm_df['symbol'] = norm_df['symbol'].str.extract(r'\((.*?)\)')
         
         assert type(norm_df) == pd.DataFrame
-        return norm_df.to_csv('wjsgainers_norm.csv', index=False)
+        return norm_df.to_csv('wsjgainers_norm.csv', index=False)
     
     else:
         print('Please enter a supported file')
--- tests/test_normalize_csv.py
+++ tests/test_normalize_csv.py
@@ -1,6 +1,18 @@
 import sys
-sys.path.append('bin/normalize_csv.py')
+import pandas as pd
+sys.path.append('.')
+import bin.normalize_csv
 
- def test_1():
-    # your code goes here
-    assert 1 == 1
+def test_normalize_ygainers():
+    ygain_df = pd.read_csv('ygainers_norm.csv')
+
+    # check column names
+    correct_cols = ['symbol', 'price', 'price_change', 'price_percent_change']
+    for col in ygain_df.columns:
+        assert col in correct_cols, f'{col} is not an expected column name'
+
+    # check number of columns
+    assert len(ygain_df.columns) == 4, 'too many columns, expects 4'
+
+def test_normalize_wjsgainers():
+
