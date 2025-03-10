03566183cd81264ef5a03b77a62d6e0cea1e80d6
tribbens <tribbens@live.com>
2025-03-10 08:15:48 +0000

    got crontab working

--- bin/crontab_csv.py
+++ bin/crontab_csv.py
@@ -1,12 +1,16 @@
 import os
 from datetime import datetime
 from zoneinfo import ZoneInfo
+
 # use ZoneInfo to set time zone to EST
 now = datetime.now(ZoneInfo("America/New_York"))
 date = str(now.date())
 time = str(now.time()).replace(':', '-')
 
-filename = 'ygainers_' + date + '_at_' + time[:-10] + '.csv'
-print(filename)
-# rename file and move it to proper folder
-os.rename('ygainers.csv', 'collected_data/' +  filename)
+filename_ygain = 'ygainers_' + date + '_at_' + time[:-10] + '.csv'
+filename_wsj = 'wsjgainers_' + date + '_at_' + time[:-10] + '.csv'
+# rename files and move them to proper folders
+os.rename('ygainers.csv', 'collected_data/' +  filename_ygain)
+os.rename('wsjgainers.csv', 'collected_data/' + filename_wsj)
+
+
