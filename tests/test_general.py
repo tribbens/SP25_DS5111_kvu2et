fd1cccc6c06e2402d441b79511a51b5748ecc235
Efrain Olivares <99214069+EfrainOlivaresUVA@users.noreply.github.com>
2025-04-02 17:01:31 -0700

    Update test_general.py

--- tests/test_general.py
+++ tests/test_general.py
@@ -7,5 +7,5 @@ print(type(sys.version))
 def test_os():
     assert platform.system() == "Linux", "wrong operating system, use a Linux system"
 
-def test_python():
+def test_python(): # head's up that this will have to change when you update the validations.yaml
     assert sys.version[:4] in ["3.10", "3.11", "3.12"], "wrong version of python, use 3.10, 3.11, or 3.12"
