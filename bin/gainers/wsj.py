2374ec08ca21a5cba5137886125a83292f5c60f0
tribbens <tribbens@live.com>
2025-04-10 00:24:16 +0000

    adding functionality to wsj.py and yahoo.py

--- bin/gainers/wsj.py
+++ bin/gainers/wsj.py
@@ -8,12 +8,21 @@ class GainerDownloadWSJ(GainerDownload):
     def download(self):
         print("Downloading WSJ gainers")
 
+
 class GainerProcessWSJ(GainerProcess):
     def __init__(self):
         pass
 
     def normalize(self):
         print("Normalizing WSJ gainers")
+	raw_df = pd.read_csv(raw_csv_path)
+        norm_df = raw_df[['Unnamed: 0', 'Last', 'Chg', '% Chg']]
+        norm_df.columns = ['symbol', 'price', 'price_change', 'price_percent_change']
+        # extract only the symbol
+        norm_df['symbol'] = norm_df['symbol'].str.extract(r'\((.*?)\)')
+
+        assert isinstance(norm_df, pd.DataFrame)
+        return norm_df.to_csv('wsjgainers_norm.csv', index=False)
 
     def save_with_timestamp(self):
         print("Saving WSJ gainers")
--- bin/gainers/yahoo.py
+++ bin/gainers/yahoo.py
@@ -5,9 +5,9 @@ class GainerDownloadYahoo(GainerDownload):
     def __init__(self):
         pass
         
-
     def download(self):
         print("Downloading yahoo gainers")
+
 
 class GainerProcessYahoo(GainerProcess):
     def __init__(self):
@@ -15,6 +15,12 @@ class GainerProcessYahoo(GainerProcess):
 
     def normalize(self):
         print("Normalizing yahoo gainers")
+	raw_df = pd.read_csv(raw_csv_path)
+        norm_df = raw_df[['Symbol', 'Price', 'Change', 'Change %']]
+        norm_df.columns = ['symbol', 'price', 'price_change', 'price_percent_change']
+
+        assert isinstance(norm_df, pd.DataFrame)
+        return norm_df.to_csv('ygainers_norm.csv', index=False)
 
     def save_with_timestamp(self):
         print("Saving Yahoo gainers")
