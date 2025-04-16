9583efaf18ec0e4a51e9775027c3677154ee7cdf
tribbens <tribbens@live.com>
2025-04-16 12:16:49 +0000

    Got all tests working and lint above 9!

--- tests/test_normalize_csv.py
+++ tests/test_normalize_csv.py
@@ -5,8 +5,8 @@ import glob
 
 # Add the path to bin/ to import gainers modules
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'bin')))
-
 from gainers.factory import GainerFactory
+
 
 def test_normalize_ygainers():
     # normalize test data from the data folder without downloading
@@ -26,6 +26,10 @@ def test_normalize_ygainers():
     # check number of columns
     assert len(ygain_df.columns) == 4, 'too many columns, expects 4'
 
+    # delete file when done
+    os.remove(match[0])
+
+
 def test_normalize_wsjgainers():
     factory = GainerFactory('wsj', 'tests/data/wsj_test_raw.csv')
     normalizer = factory.get_processor()
@@ -43,3 +47,26 @@ def test_normalize_wsjgainers():
     # check number of columns
     assert len(wsj_df.columns) == 4, 'too many columns, expects 4'
 
+    # delete file when done
+    os.remove(match[0])
+
+
+def test_normalize_sagainers():
+    factory = GainerFactory('sa', 'tests/data/sa_test_raw.csv')
+    normalizer = factory.get_processor()
+    normalizer.normalize()
+    normalizer.save_with_timestamp()
+
+    match = glob.glob("sagain*.csv")
+    wsj_df = pd.read_csv(match[0])
+
+    # check column names
+    correct_cols = ['symbol', 'price', 'price_change', 'price_percent_change']
+    for col in wsj_df.columns:
+        assert col in correct_cols, f'{col} is not an expected column name'
+
+    # check number of columns
+    assert len(wsj_df.columns) == 4, 'too many columns, expects 4'
+
+    # delete file when done
+    os.remove(match[0])
